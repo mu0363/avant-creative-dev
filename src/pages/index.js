fcd16c45d14b546e6b1351fe9fd16e90b7304bfb
John <johndoe@example.com>
2021-05-23 13:43:42 +0900

    🎉 Let's get started

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook 2.0</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <p>Welcome to Tailwind</p>
+    </div>
+  );
+}
