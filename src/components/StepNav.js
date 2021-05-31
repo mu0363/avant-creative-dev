1606ec7d32a6c60180241375297f9bb999c86f8e
John <johndoe@example.com>
2021-05-31 16:02:39 +0900

    🚧 stepper day1

--- src/components/StepNav.js
+++ src/components/StepNav.js
@@ -1,19 +1,51 @@
-import { CheckCircleIcon as CIOutline } from '@heroicons/react/outline';
-import { CheckCircleIcon as CISolid } from '@heroicons/react/solid';
+import { CheckIcon } from '@heroicons/react/solid';
 
-export const StepNav = ({ check, stepNum }) => {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export const StepNav = ({ stepNum, currentIndex, status }) => {
   return (
-    <div>
-      {check ? (
-        <div className='grid place-items-center'>
-          <CISolid className='h-6 sm:h-8 text-blue-400' />
-          <p className='text-gray-400 text-xs sm:text-base'>{`Step${stepNum}`}</p>
+    <div
+      className={classNames(
+        currentIndex !== stepNum - 1 ? 'pr-8 sm:pr-20' : '',
+        'relative'
+      )}
+    >
+      {stepNum < currentIndex ? (
+        <div>
+          <div
+            className='absolute inset-0 flex items-center'
+            aria-hidden='true'
+          >
+            <div className='h-0.5 w-full bg-indigo-600' />
+          </div>
+          <a
+            href='#'
+            className='relative w-7 h-7 flex items-center justify-center bg-blue-400 rounded-full hover:bg-indigo-900 p-1'
+          >
+            <CheckIcon className='w-7 h-7 text-white' />
+          </a>
         </div>
       ) : (
-        <div className='grid place-items-center'>
-          <CIOutline className='h-6 sm:h-8 text-gray-300' />
-          <p className='text-gray-300 text-xs sm:text-base'>{`Step${stepNum}`}</p>
-        </div>
+        <>
+          <div
+            className='absolute inset-0 flex items-center'
+            aria-hidden='true'
+          >
+            <div className='h-0.5 w-full bg-gray-200' />
+          </div>
+          <a href='#' className='relative'>
+            <p className='bg-gray-300 text-white h-7 w-7 flex items-center justify-center rounded-full font-semibold'>
+              {stepNum + 1}
+            </p>
+          </a>
+        </>
+        // <div className='grid place-items-center'>
+        //   <p className='bg-gray-300 text-white h-7 w-7 flex items-center justify-center rounded-full font-semibold'>
+        //     {stepNum + 1}
+        //   </p>
+        // </div>
       )}
     </div>
   );
