34e9c54ee275afd89ee525aa72afc568caf99711
mu0363 <dde036@gmail.com>
2021-06-07 12:38:45 +0900

    ➕ React dropzoneを追加

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,4 +1,6 @@
 import { useRef, useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { CloudUploadIcon } from '@heroicons/react/outline';
 import { PhotographIcon } from '@heroicons/react/solid';
 import { XIcon } from '@heroicons/react/solid';
 import ReactCrop from 'react-image-crop';
@@ -7,6 +9,7 @@ import 'react-image-crop/dist/ReactCrop.css';
 import { ImageModal } from 'src/components/ImageModal';
 
 export const InputBox = () => {
+  //Crop設定
   const [image, setImage] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [completedCrop, setCompletedCrop] = useState(null);
@@ -77,24 +80,53 @@ export const InputBox = () => {
     setShowModal(true);
   };
 
+  //Dropzone設定
+  const [preview, setPreview] = useState(null);
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+    setShowModal(true);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+    multiple: false,
+  });
+
   return (
-    <div className='w-full'>
-      <div className='bg-white p-3 rounded-lg mb-2'>
-        <div onClick={onFilePicker}>
-          <PhotographIcon className='h-10 text-green-500 hover:bg-gray-200 rounded-full p-2 cursor-pointer' />
-          <input
-            type='file'
-            accept='image/*'
-            hidden
-            ref={filePickerRef}
-            onChange={addImage}
-          />
+    <div className="w-full">
+      <div className="bg-white p-3 rounded-lg mb-2">
+        <div
+          {...getRootProps()}
+          className={`bg-white border-gray-300 cursor-pointer border border-dashed aspect-w-16 aspect-h-9 outline-none rounded-md ${
+            isDragActive && 'border-green-400'
+          } ${preview && 'border'}`}
+        >
+          <input {...getInputProps()} />
+          {preview ? (
+            <div className="aspect-w-16 aspect-h-9">
+              <img className="object-contain p-2" src={preview} alt="preview" />
+            </div>
+          ) : (
+            <div className="flex justify-center items-center flex-col space-y-4">
+              <p className="text-gray-400">Drag and Drop a Image here</p>
+              <CloudUploadIcon className="h-6 text-gray-400" />
+            </div>
+          )}
         </div>
+        {/* <div onClick={onFilePicker}>
+          <PhotographIcon className="h-10 text-green-500 hover:bg-gray-200 rounded-full p-2 cursor-pointer" />
+          <input type="file" accept="image/*" hidden ref={filePickerRef} onChange={addImage} />
+        </div> */}
         <form>
           <input
-            type='text'
+            type="text"
             placeholder="What's on your mind next??"
-            className='bg-gray-100 py-2 px-6 rounded-full focus:outline-none w-full'
+            className="bg-gray-100 py-2 px-6 rounded-full focus:outline-none w-full"
             ref={inputRef}
           />
           <button hidden onClick={onSubmit}>
@@ -103,7 +135,7 @@ export const InputBox = () => {
         </form>
       </div>
       {image && (
-        <div className='shadow-lg'>
+        <div className="shadow-lg">
           <canvas
             ref={previewCanvasRef}
             style={{
@@ -116,24 +148,21 @@ export const InputBox = () => {
       {showModal && (
         <div>
           <div
-            className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             // onClick={closeModal}
           >
-            <div className='relative w-auto my-6 mx-auto max-w-xl'>
+            <div className="relative w-auto my-6 mx-auto max-w-xl">
               {/*content*/}
-              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className='flex items-center justify-between p-5 border-b border-solid border-blueGray-200 rounded-t'>
-                  <XIcon
-                    className='h-6 cursor-pointer'
-                    onClick={() => setShowModal(false)}
-                  />
-                  <h3 className='text-2xl font-semibold'>Crop image here</h3>
+                <div className="flex items-center justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <XIcon className="h-6 cursor-pointer" onClick={() => setShowModal(false)} />
+                  <h3 className="text-2xl font-semibold">Crop image here</h3>
                 </div>
                 {/*body*/}
-                <div className='relative p-6 flex-auto'>
+                <div className="relative p-6 flex-auto">
                   <ReactCrop
-                    className='focus:outline-none'
+                    className="focus:outline-none"
                     src={image}
                     crop={crop}
                     onImageLoaded={onLoad}
@@ -141,17 +170,14 @@ export const InputBox = () => {
                     onComplete={(c) => setCompletedCrop(c)}
                     // onComplete={(c) => setCompletedCrop(c)}
                   />
-                  <div className='flex justify-end space-x-2 border-t p-3 mt-2'>
+                  <div className="flex justify-end space-x-2 border-t p-3 mt-2">
                     <button
-                      className='bg-gray-400 text-white px-6 py-3 rounded-md text-base'
+                      className="bg-gray-400 text-white px-6 py-3 rounded-md text-base"
                       onClick={() => setShowModal(false)}
                     >
                       Cancel
                     </button>
-                    <button
-                      className='bg-blue-400 text-white px-6 py-3 rounded-md text-base'
-                      onClick={trimmingImage}
-                    >
+                    <button className="bg-blue-400 text-white px-6 py-3 rounded-md text-base" onClick={trimmingImage}>
                       Crop
                     </button>
                   </div>
@@ -159,7 +185,7 @@ export const InputBox = () => {
               </div>
             </div>
           </div>
-          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </div>
       )}
     </div>
