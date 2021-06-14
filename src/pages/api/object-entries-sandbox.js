8fdf83cce5ba11eecc45d49d28e4b357345e3d27
mu0363 <dde036@gmail.com>
2021-06-14 20:17:46 +0900

    🐛 delete unlink api

--- src/pages/api/object-entries-sandbox.js
+++
@@ -1,9 +0,0 @@
-import { obj } from 'src/lib/obj';
-
-export default (req, res) => {
-  res.statusCode = 200;
-  const entries = Object.entries(obj).map(([key, value]) => ({
-    key: value,
-  }));
-  res.json(entries);
-};
